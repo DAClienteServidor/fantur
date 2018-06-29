7dd9f93268286effaa9e52c863322a53fc8f4b93
Nicolás Montoya <nico_m_95@hotmail.com>
2018-06-29 15:09:27 -0300

    Java Mail All

--- Fantur/src/main/java/controlador/MailController.java
+++ Fantur/src/main/java/controlador/MailController.java
@@ -1,18 +1,37 @@
 package controlador;
 
+import dao.UsuarioInterface;
+import java.util.List;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import mail.MailSender;
-
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import modelo.Usuario;
 
 @ManagedBean(name = "mailControl")
 @SessionScoped
 public class MailController {
-    
+
     private String titulo;
     private String mensaje;
     private String destinatarios;
     private String check;
+
+    @EJB
+    private UsuarioInterface ejbUsuario;
+    private List<Usuario> usuario;
+
+    @PostConstruct
+    public void init() {
+        usuario = ejbUsuario.findAll();
+        //this.paq = null;
+    }
 
     public String getCheck() {
         return check;
@@ -45,13 +64,53 @@ public class MailController {
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
     }
-    
-    public void send(){
+
+    public void send() {
         try {
-            MailSender mailSender = new MailSender();
-            mailSender.sendMail(titulo, mensaje, destinatarios);
+            String emisor = "fantur.turismo@gmail.com";
+            String clave = "fantur18";
+
+            Properties props = System.getProperties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(true);
+
+            Usuario items;
+
+            if (check.equals("Si")) {
+                for (int i = 0; i < this.usuario.size(); i++) {
+                    
+                    items = this.usuario.get(i);
+                    
+                    Message mailMessage = new MimeMessage(mailSession);
+                    mailMessage.setFrom(new InternetAddress(emisor));
+                    mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(items.getEmail()));
+                    mailMessage.setContent(this.mensaje, "text/html");
+                    mailMessage.setSubject(this.titulo);
+
+                    Transport transport = mailSession.getTransport("smtp");
+                    transport.connect("smtp.gmail.com", emisor, clave);
+                    transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
+                }
+            } else {
+                Message mailMessage = new MimeMessage(mailSession);
+                mailMessage.setFrom(new InternetAddress(emisor));
+                mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(this.destinatarios));
+                mailMessage.setContent(this.mensaje, "text/html");
+                mailMessage.setSubject(this.titulo);
+
+                Transport transport = mailSession.getTransport("smtp");
+                transport.connect("smtp.gmail.com", emisor, clave);
+                transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
+            }
         } catch (Exception e) {
         }
     }
-    
+
 }
